11c7d6fea393427bb81cf70c285d621c794640cc
Chris Mckenzie and Rob Jones <chrismckenzie+robertjones@notonthehighstreet.com>
2016-09-20 16:48:15 +0100

    wip

--- example.js
+++ example.js
@@ -20,24 +20,47 @@ var UserGist = React.createClass({
   },
 
   componentWillMount: function() {
-    fetch('https://api.github.com/repos/notonthehighstreet/notonthehighstreet/commits?sha=production_uk&access_token=a3b7da383c2bbb537923939afbb75e30438f94da')
-  .then(function(response) {
+    //since=YYYY-MM-DD
+    // for ( const key in this.state.commits ) {
+    //   console.log(this.state.commits[key]);
+    // }
+    let commitDate = this.getDateFromDaysAgo(14);
+    let url = 'https://api.github.com/repos/notonthehighstreet/notonthehighstreet/commits?sha=production_uk&since=' + commitDate + '&access_token=49783e8b119452a0d226ab69840221788c143c84'
+    fetch(url)
+  .then((response) => {
       if (response.status >= 400) {
           throw new Error("Bad response from server");
       }
       return response.json();
   })
-  .then(function(stories) {
-      console.log(stories);
-      // set state to can render true;
+  .then((commits) => {
+    console.log(commits);
+    this.setState({commits: commits});
+    // set state to can render true;
   });
+  },
+
+  getDateFromDaysAgo: function(days) {
+    const dayInMillis = 8.64e7;
+    const daysAgo = dayInMillis*days;
+    const commitDate = new Date(+new Date - daysAgo);
+
+    const year = commitDate.getFullYear();
+    const month = ('0' + (commitDate.getMonth() + 1)).slice(-2);
+    const day = ('0' + commitDate.getDate()).slice(-2);
+
+    return year + "-" + month + "-" + day;
   },
 
   render: function() {
     // render only if can render true
+    let commits = this.state.commits.map((commit, i) => {
+      return <Commit key={i} author={author.name} date={author.date} />
+    })
+
     return (
       <div>
-        yo....!
+        {commits}
       </div>
     );
   }
